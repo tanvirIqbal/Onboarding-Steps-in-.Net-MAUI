9f1b4ae4eabd24700a1537208876443c43766f4c
tanviriqbal <tanviriqbal4u@live.com>
2023-06-18 21:12:18 +0600

    Initial Commit - Project Creation

---
+++ OnboardingAppDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OnboardingAppDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ OnboardingAppDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OnboardingAppDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
